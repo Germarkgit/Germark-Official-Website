a50b751808514ad13b038e9e7842baa50c7da524
Germarkgit <germarkcontact@gmail.com>
2025-09-22 15:29:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ import { getDatabase, ref, set, get, onValue, remove } from "https://www.gstatic
 const firebaseConfig = {
   apiKey: "AIzaSyAJkSlH4IRuIOldjQyyEWINr2sGFylQ8co",
   authDomain: "germarkdigitalportal.firebaseapp.com",
-  databaseURL: "https://germarkdigitalportal-default-rtdb.europe-west1.firebasedatabase.app/",
+  databaseURL: "https://germarkdigitalportal-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "germarkdigitalportal",
   storageBucket: "germarkdigitalportal.firebasestorage.app",
   messagingSenderId: "511878818738",
@@ -247,5 +247,8 @@ function displayNews() {
       `;
       container.appendChild(article);
     });
- 
+  });
+}
 
+// Expose functions globally
+window.login
